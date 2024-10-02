8780fed534b71e10718f7cc9eb0cf805ed36e7cf
Hojayfa Rahman <hojayfarahman@gmail.com>
2024-10-02 12:08:02 +0600

    add serializers for employee, department & achievement

--- employee_tracker/serializers.py
+++ employee_tracker/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Employee, Department, Achievement, AchievementEmployee
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,3 +30,80 @@ class LoginSerializer(serializers.Serializer):
 
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Department model.
+    """
+
+    class Meta:
+        model = Department
+        fields = ["id", "name"]
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Achievement model.
+    """
+
+    class Meta:
+        model = Achievement
+        fields = ["id", "name"]
+
+
+class AchievementEmployeeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the AchievementEmployee model, managing employee-achievement relationships.
+    """
+
+    achievement = AchievementSerializer(read_only=True)
+    achievement_id = serializers.PrimaryKeyRelatedField(
+        queryset=Achievement.objects.all(), source="achievement", write_only=True
+    )
+
+    class Meta:
+        model = AchievementEmployee
+        fields = ["id", "achievement", "achievement_id", "achievement_date"]
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Employee model.
+    """
+
+    department = DepartmentSerializer(read_only=True)
+    department_id = serializers.PrimaryKeyRelatedField(
+        queryset=Department.objects.all(), source="department", write_only=True
+    )
+    achievements = AchievementEmployeeSerializer(
+        source="achievementemployee_set", many=True, read_only=True
+    )
+
+    class Meta:
+        model = Employee
+        fields = [
+            "id",
+            "name",
+            "email",
+            "phone",
+            "address",
+            "department",
+            "department_id",
+            "achievements",
+        ]
+
+    def create(self, validated_data):
+        achievements_data = self.context["request"].data.get("achievements", [])
+        employee = Employee.objects.create(**validated_data)
+        for achievement_data in achievements_data:
+            AchievementEmployee.objects.create(employee=employee, **achievement_data)
+        return employee
+
+    def update(self, instance, validated_data):
+        achievements_data = self.context["request"].data.get("achievements", [])
+        instance = super().update(instance, validated_data)
+        instance.achievementemployee_set.all().delete()
+        for achievement_data in achievements_data:
+            AchievementEmployee.objects.create(employee=instance, **achievement_data)
+        return instance
