a1cf983eb464c3abd243c628131a2348ac133a71
Hojayfa Rahman <hojayfarahman@gmail.com>
2024-10-02 11:25:44 +0600

    create employee, department & achievement models

--- employee_tracker/models.py
+++ employee_tracker/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    """
+    Represents a department in the organization.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    """
+    Represents an employee in the organization.
+    """
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    achievements = models.ManyToManyField("Achievement", through="AchievementEmployee")
+
+    def __str__(self):
+        return self.name
+
+
+class Achievement(models.Model):
+    """
+    Represents an achievement that can be awarded to employees.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AchievementEmployee(models.Model):
+    """
+    Represents the relationship between an employee and their achievements,
+    including the date the achievement was awarded.
+    """
+
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    achievement_date = models.DateField()
+
+    class Meta:
+        unique_together = ("employee", "achievement")
+
+    def __str__(self):
+        return f"{self.employee.name} - {self.achievement.name}"
